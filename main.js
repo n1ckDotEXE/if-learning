98edb887f36ef286413d37d8c2970ab20706f965
n1ckDotEXE <admin@nicholasnam.com>
2020-09-24 02:33:37 -0400

    Completed

--- main.js
+++ main.js
@@ -2,10 +2,45 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+function isItLong(val1) {
+  if (val1.length > 20) {
+    return "That's a long string!";
+  }
+}
 
+function isItMedium(val1) {
+  if (val1.length <= 20 && val1.length >= 10) {
+    return "That's a regular sized string!";
+  }
+}
 
+function isItShort(val1) {
+  if (val1.length < 10) {
+    return "That's a small string!";
+  } else {
+    return "That's not a small string!";
+  }
+}
 
+function howLongIsMyString(val1) {
+  if (val1.length > 20) {
+    return "That's a long string!";
+  } else if (val1.length <= 20 && val1.length >= 10) {
+    return "That's a regular sized string!";
+  } else { (val1.length < 10)
+    return "That's a small string!";
+  }
+}
 
+function instructorHeight(val1) {
+  if (val1 === "Colin") {
+    return 62;
+  } else if (val1 === "Mesuara") {
+    return 69;
+  } else { 
+    return "I don't know that instructor!";
+  }
+}
 
 /**********************************
  *      OUR CODE GOES BELOW.      *
